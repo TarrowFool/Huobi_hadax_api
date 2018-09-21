3dd7da525e7888f4b728ed1289957ead076166c0
TarrowFool <18636056836@163.com>
2018-09-21 09:42:14 +0800

    Update
    Update

--- demo.php
+++ demo.php
@@ -2,9 +2,9 @@
 
 header("Content-Type:text/html;charset=utf-8");
 // 定义参数
-define('ACCOUNT_ID', '52914421'); // 你的账户ID
-define('ACCESS_KEY','128e9949-b83b5597-ad4adbfb-cb3f7'); // 你的ACCESS_KEY
-define('SECRET_KEY','d8a521b4-742292d5-664eccc7-15d6a'); // 你的SECRET_KEY
+define('ACCOUNT_ID', ''); // 你的账户ID
+define('ACCESS_KEY',''); // 你的ACCESS_KEY
+define('SECRET_KEY',''); // 你的SECRET_KEY
 
 include "lib.php";
 
@@ -14,11 +14,6 @@ $req = new req();
 $data = $req->get_detail_merged('btcusdt');
 
 $array = json_decode(json_encode($data),TRUE);
-
-echo "<pre>";
-print_r($array);
-echo "</pre>";
-
 
 echo "最新收盘价：BTC-USDT: ".$array['tick']['close'];
 
